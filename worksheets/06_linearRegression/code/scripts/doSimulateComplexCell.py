b1e8b93b8f637c73fe826a84c5fa781e58fd6abe
Joaquin Rapela <joacorapela@gmail.com>
2024-02-21 11:21:30 +0000

    Several updates

--- worksheets/06_linearRegression/code/scripts/doSimulateComplexCell.py
+++ worksheets/06_linearRegression/code/scripts/doSimulateComplexCell.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 import numpy as np
 import pandas as pd
+import plotly.subplots
 import plotly.graph_objects as go
 
 import utils
@@ -15,13 +16,13 @@ def main(argv):
                         default=1.0)
     parser.add_argument("--images_filename", type=str,
                         help="images filename",
-                        default="http://www.gatsby.ucl.ac.uk/~rapela/neuroinformatics/2024/worksheets/linearRegression/data/xCC.dat")
+                        default="../..//data/equalpower_C2_25hzPP.dat")
     parser.add_argument("--responses_filename", type=str,
                         help="simulated responses filename",
                         default="../../results/ySimCC.dat")
     parser.add_argument("--figures_filename_pattern", type=str,
                         help="figures filename pattern",
-                        default="../../figures/ySimCC.{:s}")
+                        default="../../figures/simCC_{:s}.{:s}")
     args = parser.parse_args()
 
     # get command line parameters
@@ -31,32 +32,51 @@ def main(argv):
     figures_filename_pattern = args.figures_filename_pattern
 
     # get data
-    images = pd.read_csv(images_filename, sep=" ").to_numpy()
-
-    # get noise from snr
-    std_noise = np.mean(np.abs(images)) / snr
+    images = pd.read_csv(images_filename, sep="\s+", header=None).to_numpy()
+    image_width = image_height = int(np.sqrt(images.shape[1]))
 
     # compute basis functions and projections of images onto them
-    gabor0 = utils.gabor(sigma=1.5, theta=np.pi/4, Lambda=1, psi=0,
-                         gamma=1).flatten()
-    gabor1 = utils.gabor(sigma=1.5, theta=np.pi/4, Lambda=1, psi=np.pi/2,
-                         gamma=1).flatten()
+    gabor0 = utils.gabor(sigma=2.5, theta=-np.pi/4, Lambda=1, psi=0,
+                         gamma=1)
+    gabor0 = gabor0[:12, :12].flatten()
+    gabor1 = utils.gabor(sigma=2.5, theta=-np.pi/4, Lambda=1, psi=np.pi/2,
+                         gamma=1)
+    gabor1 = gabor1[:12, :12].flatten()
+
     rds = np.zeros(shape=(images.shape[1], 2), dtype=np.double)
-    rds[0:len(gabor0), 0] = gabor0
-    rds[0:len(gabor1), 1] = gabor1
+    rds[:len(gabor0), 0] = gabor0.flatten()
+    rds[:len(gabor1), 1] = gabor1.flatten()
 
     # simulate responses
-    noise = np.random.normal(loc=0, scale=std_noise, size=images.shape[0])
     noiseless_responses = (images @ rds[:, 0])**2 + (images @ rds[:, 1])**2
+
+    max_firing_rate = 100
+    noiseless_responses *= max_firing_rate / np.max(noiseless_responses)
+
+    # get noise from snr
+    std_noise = np.mean(noiseless_responses) / snr
+    noise = np.random.normal(loc=0, scale=std_noise, size=images.shape[0])
+
     responses = noiseless_responses + noise
     responses[responses < 0] = 0.0
 
     # save responses
     df = pd.DataFrame(responses)
-    df.to_csv(responses_filename)
+    df.to_csv(responses_filename, header=None, index=False)
     print(f"Simulation written to {responses_filename}")
 
-    # plot responses
+    # plot
+    # RDs
+    fig = plotly.subplots.make_subplots(rows=1, cols=2)
+    for i in range(2):
+        alpha = np.reshape(rds[:, i], newshape=(image_width, image_height))
+        trace = go.Heatmap(z=alpha)
+        fig.add_trace(trace, row=1, col=i+1)
+    fig.write_image(figures_filename_pattern.format("rds", "png"))
+    fig.write_html(figures_filename_pattern.format("rds", "html"))
+    fig.show()
+
+    # responses
     fig = go.Figure()
     trace = go.Scatter(x=np.arange(len(responses)), y=responses,
                        mode="markers", name="responses")
@@ -66,8 +86,8 @@ def main(argv):
                        mode="markers", name="noiseless_responses")
     fig.add_trace(trace)
     fig.update_layout(xaxis=dict(title="Index"), yaxis=dict(title="Response"))
-    fig.write_image(figures_filename_pattern.format("png"))
-    fig.write_html(figures_filename_pattern.format("html"))
+    fig.write_image(figures_filename_pattern.format("responses", "png"))
+    fig.write_html(figures_filename_pattern.format("responses", "html"))
     fig.show()
 
     breakpoint()
