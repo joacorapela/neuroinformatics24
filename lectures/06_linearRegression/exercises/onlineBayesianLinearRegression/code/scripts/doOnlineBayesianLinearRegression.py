18586fabbe6eda326ee8561f3da8b45597e64b12
Joaquin Rapela <joacorapela@gmail.com>
2024-03-06 21:14:29 +0000

    A few relevant changes (e.g., delay responses)

--- lectures/06_linearRegression/exercises/onlineBayesianLinearRegression/code/scripts/doOnlineBayesianLinearRegression.py
+++ lectures/06_linearRegression/exercises/onlineBayesianLinearRegression/code/scripts/doOnlineBayesianLinearRegression.py
@@ -8,6 +8,10 @@ import joacorapela_common.stats.bayesianLinearRegression as blr
 
 def main(argv):
     parser = argparse.ArgumentParser()
+    parser.add_argument("--response_delay_samples", type=int,
+                        help=("we associate each response to the image "
+                              "presented response_delay_samples in the past"),
+                        default=1)
     parser.add_argument("--prior_precision_coef", type=float,
                         help="prior precision coefficient",
                         default=0.5)
@@ -23,6 +27,7 @@ def main(argv):
                         default="../../data/ySC_5spi.dat")
     args = parser.parse_args()
 
+    response_delay_samples = args.response_delay_samples
     prior_precision_coef = args.prior_precision_coef
     likelihood_precision_coef = args.likelihood_precision_coef
     update_delay = args.update_delay
@@ -33,13 +38,18 @@ def main(argv):
     responses = np.genfromtxt(responses_filename)
     Phi = np.column_stack((np.ones(len(images)), images))
     image_width = int(np.sqrt(images.shape[1]))
-    image_heigth = image_width
+    image_height = image_width
+
+    Phi = Phi[:-response_delay_samples,]
+    responses = responses[response_delay_samples:]
 
     m0 = np.zeros(shape=(images.shape[1]+1,), dtype=np.double)
     S0 = np.diag(np.ones(len(m0), dtype=np.double))
     indices = np.arange(len(m0))
 
-    fig, ax = plt.subplots(nrows=2, ncols=1)
+    fig = plt.figure()
+    ax1 = fig.add_subplot(2, 1, 1, adjustable="box", aspect=1)
+    ax2 = fig.add_subplot(2, 1, 2)
 
     mn = m0
     Sn = S0
@@ -48,21 +58,22 @@ def main(argv):
                                   alpha=prior_precision_coef,
                                   beta=likelihood_precision_coef)
         stds = np.sqrt(np.diag(Sn))
-        ax[0].clear()
-        ax[0].imshow(mn[1:].reshape((image_width, image_heigth)))
+        ax1.clear()
+        ax1.contourf(mn[1:].reshape((image_width, image_height)))
         title = (r"$\alpha={:.02f},\beta={:.02f},\lambda={:.02f},"
                  "{:d}/{:d}$".format(
                      prior_precision_coef, likelihood_precision_coef,
                      prior_precision_coef/likelihood_precision_coef,
                      n, len(responses)))
-        ax[0].set_title(title)
-        ax[1].clear()
-        ax[1].errorbar(x=indices, y=mn, yerr=1.96*stds)
-        ax[1].axhline(y=0)
-        ax[1].set_xlabel("Pixel index")
-        ax[1].set_ylabel("Pixel value")
+        ax1.set_title(title)
+        ax2.clear()
+        ax2.errorbar(x=indices, y=mn, yerr=1.96*stds)
+        ax2.axhline(y=0)
+        ax2.set_xlabel("Pixel index")
+        ax2.set_ylabel("Pixel value")
         # Note that using time.sleep does *not* work here!
         plt.pause(update_delay)
+
     breakpoint()
 
 
